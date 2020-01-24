ab5b404a16b8152cc7853e27add62eee97275be3
jynnie <jynnie@mit.edu>
2020-01-24 00:36:20 -0500

    Attempting another push with single pages

---
+++ dist/precache-manifest.97a61610714d9c8558372287d2d70ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbb1d753e98d45d0528c",
+    "url": "/static/css/main.38ccfe96.chunk.css"
+  },
+  {
+    "revision": "fbb1d753e98d45d0528c",
+    "url": "/static/js/main.fbb1d753.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a19c953d477d6da49102",
+    "url": "/static/js/2.a19c953d.chunk.js"
+  },
+  {
+    "revision": "a2977060412a2bbccc8858a398b2fec9",
+    "url": "/index.html"
+  }
+];
